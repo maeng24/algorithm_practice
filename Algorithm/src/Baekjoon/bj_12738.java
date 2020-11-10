801aea702c9cc631e72bcc03c84e9dff540eedec
maeng24 <maengindex@gmail.com>
2020-11-10 19:30:49 +0900

    주석 추가

--- Algorithm/src/Baekjoon/bj_12738.java
+++ Algorithm/src/Baekjoon/bj_12738.java
@@ -7,35 +7,48 @@ import java.util.StringTokenizer;
 
 public class bj_12738 {
 
+	static int result;
+	static int[] answer;
+	
+	//이분탐색으로 자리 찾기
+	public static void searchindex(int number) {
+		int start = 0;
+		int dest = result - 1;
+		int mid = 0;
+		while (start < dest) {
+			mid = (start + dest) >> 1;
+			if (answer[mid] < number) {
+				start = mid + 1;
+			} else {
+				dest = mid;
+			}
+		}
+		answer[dest] = number;
+	}
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int len = Integer.parseInt(br.readLine());
 
 		int number = 0;
-		int[] answer = new int[len];
+		answer = new int[len];
 
 		StringTokenizer tk = new StringTokenizer(br.readLine(), " ");
 
-		int result = 1;
+		result = 1;
 		answer[0] = Integer.parseInt(tk.nextToken());
 
 		for (int i = 1; i < len; i++) {
 			number = Integer.parseInt(tk.nextToken());
 			if (answer[result - 1] < number) {
-				answer[result] = number;
-				result++;
-			} else {
-				for (int j = result - 2; j >= 0; j--) {
-					if (answer[j] < number) {
-						answer[j + 1] = number;
-						break;
-					}
-				}
+				answer[result++] = number;
+			} else {//다음 인덱스에 넣을 수 없다면 맞는 자리를 찾는다.
+				searchindex(number);
 			}
 		}
 
 		System.out.println(result);
 	}
-
+	
 }
