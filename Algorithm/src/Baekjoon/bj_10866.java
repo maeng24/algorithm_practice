bfac7eb6bb7ada29c2e8b5b8395345133ba02ce5
maeng24 <maengindex@gmail.com>
2020-12-20 18:46:49 +0900

    백준 10866번 - 덱

---
+++ Algorithm/src/Baekjoon/bj_10866.java
@@ -0,0 +1,74 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class bj_10866 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int num=Integer.parseInt(br.readLine());
+		Deque<Integer> dq=new LinkedList<>();
+		StringTokenizer tk=null;
+		for (int i = 0; i < num; i++) {
+			tk=new StringTokenizer(br.readLine());
+			String str=tk.nextToken();
+			if (str == "push_front") {
+				int n=Integer.parseInt(tk.nextToken());
+				dq.addFirst(n);
+			}
+			else if (str == "push_back") {
+				int n=Integer.parseInt(tk.nextToken());
+				dq.addLast(n);
+			}
+			else if (str == "pop_front") {
+				if (dq.isEmpty()) {
+					System.out.println(-1);
+				}
+				else {
+					System.out.println(dq.pollFirst());
+				}
+			}
+			else if (str == "pop_back") {
+				if (dq.isEmpty()) {
+					System.out.println(-1);
+				}
+				else {
+					System.out.println(dq.pollLast());
+				}
+			}
+			else if (str == "size") {
+				System.out.println(dq.size());
+			}
+			else if (str == "empty") {
+				if (dq.isEmpty()) {
+					System.out.println(1);
+				}
+				else {
+					System.out.println(0);
+				}
+			}
+			else if (str == "front") {
+				if (dq.isEmpty()) {
+					System.out.println(-1);
+				}
+				else {
+					System.out.println(dq.peekFirst());
+				}
+			}
+			else if (str == "back") {
+				if (dq.isEmpty()) {
+					System.out.println(-1);
+				}
+				else {
+					System.out.println(dq.peekLast());
+				}
+			}
+		}
+	}
+
+}
