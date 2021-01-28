c258dcf49c1e7e94ff6a2b6020a3a30f5e980f2f
maeng24 <maengindex@gmail.com>
2021-01-28 18:56:33 +0900

    수정

--- Algorithm/src/Baekjoon/bj_5427.java
+++ Algorithm/src/Baekjoon/bj_5427.java
@@ -8,6 +8,9 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class bj_5427 {
+	public static char[][] map;
+	public static boolean[][] visit;
+	public static Queue<int[]> fire;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		int[] movex = { 1, 0, -1, 0 };
@@ -18,12 +21,13 @@ public class bj_5427 {
 		int t = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < t; i++) {
+			System.out.println(i);
 			StringTokenizer tk = new StringTokenizer(br.readLine(), " ");
 			int x = Integer.parseInt(tk.nextToken());
 			int y = Integer.parseInt(tk.nextToken());
-			char[][] map = new char[y][x];
-			Queue<int[]> fire = new LinkedList<int[]>();
-			Queue<int[]> people = new LinkedList<int[]>();
+			map = new char[y][x];
+			visit = new boolean[y][x];
+			fire = new LinkedList<int[]>();
 			boolean flag = true;
 
 			for (int j = 0; j < y; j++) {
@@ -31,45 +35,45 @@ public class bj_5427 {
 				for (int k = 0; k < x; k++) {
 					map[j][k] = str.charAt(k);
 					if (map[j][k] == '*') {
-						fire.add(new int[] { j, k });
+						fire.add(new int[] { j, k,0 });//불
 					} else if (map[j][k] == '@') {
-						people.add(new int[] { j, k });
+						fire.add(new int[]{j,k,1});//사람
 					}
 				}
 			}
 
 			int count = 0;
-
-			while (!people.isEmpty()) {
+			while (!fire.isEmpty()) {
 				count++;
 				int firelen = fire.size();
 				for (int j = 0; j < firelen; j++) {
 					int firey = fire.peek()[0];
-					int firex = fire.poll()[1];
-					for (int k = 0; k < 4; k++) {
-						int nextx = firex + movex[k];
-						int nexty = firey + movey[k];
-						if (nextx >= 0 && nextx < x && nexty >= 0 && nexty < y && map[nexty][nextx] != '*'
-								&& map[nexty][nextx] != '#') {
-							map[nexty][nextx] = '*';
-							fire.add(new int[] { nexty, nextx });
+					int firex = fire.peek()[1];
+					int forp=fire.poll()[2];
+					// 상근이일 경우
+					if (forp== 1) {
+						for (int k = 0; k < 4; k++) {
+							int nextx = firex + movex[k];
+							int nexty = firey + movey[k];
+							if (nextx < 0 || nexty < 0 || nextx >= x || nexty >= y) {
+								flag = false;
+								break;
+							} else if (nextx >= 0 && nextx < x && nexty >= 0 && nexty < y && map[nexty][nextx] != '*'
+									&& map[nexty][nextx] != '#'&&!visit[nexty][nextx]) {
+								fire.add(new int[] { nexty, nextx,1 });
+								visit[nexty][nextx]=true;
+							}
 						}
-					}
-				}
+					} else {// 불일 경우
+						for (int k = 0; k < 4; k++) {
+							int nextx = firex + movex[k];
+							int nexty = firey + movey[k];
 
-				int peoplelen = people.size();
-				for (int j = 0; j < peoplelen; j++) {
-					int peopley = people.peek()[0];
-					int peoplex = people.poll()[1];
-					for (int k = 0; k < 4; k++) {
-						int nextx = peoplex + movex[k];
-						int nexty = peopley + movey[k];
-						if (nextx < 0 || nexty < 0 || nextx >= x || nexty >= y) {
-							flag = false;
-							break;
-						} else if (nextx >= 0 && nextx < x && nexty >= 0 && nexty < y && map[nexty][nextx] != '*'
-								&& map[nexty][nextx] != '#') {
-							people.add(new int[] { nexty, nextx });
+							if (nextx >= 0 && nextx < x && nexty >= 0 && nexty < y && map[nexty][nextx] != '*'
+									&& map[nexty][nextx] != '#') {
+								map[nexty][nextx] = '*';
+								fire.add(new int[] { nexty, nextx,0 });
+							}
 						}
 					}
 					if (!flag) {
@@ -79,6 +83,7 @@ public class bj_5427 {
 				if (!flag) {
 					break;
 				}
+				
 			}
 			if (!flag) {
 				System.out.println(count);
@@ -87,5 +92,4 @@ public class bj_5427 {
 			}
 		}
 	}
-
 }
