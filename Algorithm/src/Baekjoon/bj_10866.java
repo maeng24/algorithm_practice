4790365d01b6d6fbae47e82736971371f76d9980
maeng24 <maengindex@gmail.com>
2021-03-01 21:54:56 +0900

    수정

--- Algorithm/src/Baekjoon/bj_10866.java
+++ Algorithm/src/Baekjoon/bj_10866.java
@@ -1,6 +1,7 @@
 package Baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Deque;
 import java.util.LinkedList;
@@ -8,12 +9,14 @@ import java.util.StringTokenizer;
 
 public class bj_10866 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int num=Integer.parseInt(br.readLine());
 		Deque<Integer> dq=new LinkedList<>();
 		StringTokenizer tk=null;
+		
+		//
 		for (int i = 0; i < num; i++) {
 			tk=new StringTokenizer(br.readLine());
 			String str=tk.nextToken();
