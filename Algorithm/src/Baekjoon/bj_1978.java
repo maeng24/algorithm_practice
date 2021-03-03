ebb15aca69397ceea1203e410f12dba2c4b9e4ee
maeng24 <maengindex@gmail.com>
2021-03-03 16:26:22 +0900

    백준 1978번 - 소수 찾기

---
+++ Algorithm/src/Baekjoon/bj_1978.java
@@ -0,0 +1,37 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_1978 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		StringTokenizer tk = new StringTokenizer(br.readLine(), " ");
+
+		int n = 0, answer = 0;
+		boolean primenum = false;
+		for (int i = 0; i < num; i++) {
+			n = Integer.parseInt(tk.nextToken());
+			if (n > 1) {
+				primenum = true;
+			}
+			for (int j = 2, k = n / 2 + 1; j < k; j++) {
+				if (n % j == 0) {
+					primenum = false;
+					break;
+				}
+			}
+			if (primenum) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
+
+	}
+
+}
