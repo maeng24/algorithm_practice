d5f30a24bd630a29f089048f9bac2ef47ad6b77e
maeng24 <maengindex@gmail.com>
2021-03-04 15:47:51 +0900

    주석 추가

--- Algorithm/src/Baekjoon/bj_1978.java
+++ Algorithm/src/Baekjoon/bj_1978.java
@@ -17,9 +17,11 @@ public class bj_1978 {
 		boolean primenum = false;
 		for (int i = 0; i < num; i++) {
 			n = Integer.parseInt(tk.nextToken());
+			//n이 1보다 클때만 계산
 			if (n > 1) {
 				primenum = true;
 			}
+			//소수인지 아닌지 판별한다.
 			for (int j = 2, k = n / 2 + 1; j < k; j++) {
 				if (n % j == 0) {
 					primenum = false;
