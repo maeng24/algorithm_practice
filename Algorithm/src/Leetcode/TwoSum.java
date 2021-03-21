c654ffb0370710f1deee180a0b1c97e6993eb93c
maeng24 <maengindex@gmail.com>
2021-03-21 22:51:19 +0900

    주석 추가

--- Algorithm/src/Leetcode/TwoSum.java
+++ Algorithm/src/Leetcode/TwoSum.java
@@ -4,6 +4,7 @@ public class TwoSum {
 
 	public int[] twoSum(int[] nums, int target) {
 		int len = nums.length;
+		//두 개의 합이 target이 되는 순간 리턴한다.
 		for (int i = 0; i < len; i++) {
 			for (int j = i + 1; j < len; j++) {
 				if (nums[i] + nums[j] == target) {
